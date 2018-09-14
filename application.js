68a7428b8999638717d27dea63bbdc8e73c174e7
Natalia <natalia.castillejoarias@yale.edu>
2018-09-14 17:53:30 -0400

    Added columns and new fruit

--- application.js
+++ application.js
@@ -5,9 +5,16 @@ jQuery(document).ready(function() {
     	axis: "x"
     });
 
-        $(".falling-object").delay(800).animate({bottom: 170}, 3600);
-        $(".falling-object").fadeOut("fast");
+		$(".falling-object:first").delay(800).animate({bottom: 170}, 3600);
+        $(".falling-object:first").fadeOut("fast");
 
-        $("p").text("Orange");
+        $(".falling-object:eq(2)").delay(2000).animate({bottom: 170}, 3600);
+        $(".falling-object:eq(2)").fadeOut("fast");
+
+        $(".falling-object:eq(3)").delay(3600).animate({bottom: 170}, 3600);
+        $(".falling-object:eq(3)").fadeOut("fast");
+
+        $(".falling-object:last").delay(4000).animate({bottom: 170}, 3600);
+        $(".falling-object:last").fadeOut("fast");
 
 })
